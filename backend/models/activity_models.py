24af800a59660e0e731bca69493666b73e6233d7
Praveen Patibandla <patibandlapraveen2@gmail.com>
2025-07-26 14:52:41 -0400

    created start_time feature in smart_scheduler sql db. in process of connection to Google API

--- backend/models/activity_models.py
+++ backend/models/activity_models.py
@@ -1,4 +1,5 @@
 from extensions import db
+from datetime import timedelta
 
 class Activity(db.Model):
     __tablename__ = 'activity'
@@ -6,4 +7,12 @@ class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     difficulty = db.Column(db.Integer)
-    estimated_duration = db.Column(db.Integer)
+    estimated_duration = db.Column(db.Integer)  # In minutes
+    start_time = db.Column(db.DateTime, nullable=False)
+
+    # Computed property for end time
+    @property
+    def end_time(self):
+        if self.start_time and self.estimated_duration:
+            return self.start_time + timedelta(minutes=self.estimated_duration)
+        return None
