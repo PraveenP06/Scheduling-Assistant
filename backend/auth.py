9128e952e783c42fc223d73ae74d31ca826351a2
Praveen Patibandla <patibandlapraveen2@gmail.com>
2025-08-08 15:50:46 -0400

    Events successfuly stored in Google Calendar. Also user is given multiple time slots to choose from

--- backend/auth.py
+++ backend/auth.py
@@ -1,28 +1,24 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-import os
+import os.path
 import pickle
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def get_google_credentials():
     creds = None
-    token_path = 'token.pickle'
-    creds_path = 'scheduler_credentials.json'
-
-    if os.path.exists(token_path):
-        with open(token_path, 'rb') as token:
+    if os.path.exists('token.json'):
+        with open('token.json', 'rb') as token:
             creds = pickle.load(token)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('scheduler_credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open(token_path, 'wb') as token:
+        with open('token.json', 'wb') as token:
             pickle.dump(creds, token)
 
     return creds
